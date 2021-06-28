1f9fe30a6efbe1d6ce82a0b48472deaca8d2d563
DovzhenkoDenys1989 <ddv10jan1989@gmail.com>
2021-06-28 12:11:14 +0300

    Add method check

--- index.js
+++ index.js
@@ -3,25 +3,26 @@ const fs = require('fs').promises
 
 const routerGET = {
   '/': async (req, res) => {
-    const data = await fs.readFile("./views/index.html", 'utf8')
+    const data = await fs.readFile('./views/index.html', 'utf8')
     res.end(data)
   },
   '/contacts.html': async (req, res) => {
-    const data = await fs.readFile("./views/contacts.html", 'utf8')
+    const data = await fs.readFile('./views/contacts.html', 'utf8')
     res.end(data)
   },
   '/about.html': async (req, res) => {
-    const data = await fs.readFile("./views/about.html", 'utf8')
+    const data = await fs.readFile('./views/about.html', 'utf8')
     res.end(data)
   }
 }
 
 const requestListener = async (req, res) => {
-  if(routerGET[req.url]){
-    return routerGET[req.url](req, res)
+  const { url, method } = req
+  if (method === 'GET' && routerGET[url]) {
+    return routerGET[url](req, res)
   }
 
-  const data = await fs.readFile("./views/404.html", 'utf8')
+  const data = await fs.readFile('./views/404.html', 'utf8')
   res.end(data)
 }
 
